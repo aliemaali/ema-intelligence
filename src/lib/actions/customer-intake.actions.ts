3c50f911354aeb42740ce565c17796ac9311568c
aliemaali <131735669+aliemaali@users.noreply.github.com>
2026-07-11 18:05:21 +0200

    Remove failing customer intake action wrapper

--- src/lib/actions/customer-intake.actions.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { createProject } from '@/lib/actions/project.actions'
-
-export async function createCustomerIntakeProject(formData: FormData): Promise<void> {
-  await createProject(formData)
-}
